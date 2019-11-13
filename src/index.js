719522325e79c45ad0b9929093878749ceb0f2e1
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2019-11-13 13:28:58 +0100

    Postgres setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import { config } from 'dotenv';
+import debug from 'debug';
 import express from 'express';
-import {config} from 'dotenv';
 
 config();
 
 const app = express();
+const DEBUG = debug('dev');
 const port = process.env.NODE_ENV === 'test' ? 8378 : process.env.PORT || 3000;
 
 app.use(express.json());
@@ -13,6 +15,6 @@ app.get('/', (_, res) => {
   res.status(200).json({ msg: 'Welcome to TeamWork' });
 });
 
-app.listen(port, () => console.log(`server on port ${port}`));
+app.listen(port, () => DEBUG(`server on port ${port}`));
 
 export default app;
