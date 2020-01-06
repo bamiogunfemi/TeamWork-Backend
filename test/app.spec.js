86ece2a9b87eff0f12b01e83998d6609b5e0184b
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2020-01-06 20:31:47 +0100

    created database test

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,27 +1,27 @@
-import { expect } from 'chai';
-import request from 'supertest';
-import app from '../src';
+import { expect } from "chai";
+import request from "supertest";
+import app from "../src";
 
-describe('server setup', () => {
-  it('app should be a function', () => {
-    expect(app).to.be.a('function');
+describe("server setup", () => {
+  it("app should be a function", () => {
+    expect(app).to.be.a("function");
   });
 
-  it('should redirect to docs route', (done) => {
+  it("should redirect to docs route", done => {
     request(app)
-      .get('/')
+      .get("/")
       .end((err, res) => {
         expect(res.status).to.equal(302);
         done(err);
       });
   });
 
-  it('should return error on an invalid route', (done) => {
+  it("should return error on an invalid route", done => {
     request(app)
-      .get('/404')
+      .get("/404")
       .end((err, res) => {
         expect(res.status).to.equal(404);
-        expect(res.body.error).to.eql('endpoint doesn\'t exist');
+        expect(res.body.error).to.eql("endpoint doesn't exist");
         done(err);
       });
   });
