91cef3257e1c774c07288be2f45c0247a9f98ceb
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2020-01-06 20:29:11 +0100

    modified app.spec.js

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,17 +1,28 @@
-import request from 'supertest';
 import { expect } from 'chai';
+import request from 'supertest';
 import app from '../src';
 
-describe('', () => {
-  it('', () => {
+describe('server setup', () => {
+  it('app should be a function', () => {
     expect(app).to.be.a('function');
   });
 
-  it('', () => {
+  it('should redirect to docs route', (done) => {
     request(app)
       .get('/')
       .end((err, res) => {
-        expect(res.status).to.equal(200);
+        expect(res.status).to.equal(302);
+        done(err);
+      });
+  });
+
+  it('should return error on an invalid route', (done) => {
+    request(app)
+      .get('/404')
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        expect(res.body.error).to.eql('endpoint doesn\'t exist');
+        done(err);
       });
   });
 });
